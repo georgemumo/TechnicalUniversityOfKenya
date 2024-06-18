1a23e3e9acefb5c9c832c8d7810bcb7dcf8f5cb7
georgemumo <georgemumo182@gmail.com>
2024-06-18 21:31:17 +0300

    first commit

--- app/src/main/java/com/example/techuni/ui/theme/screens/News/news.kt
+++
@@ -1,9 +0,0 @@
-package com.example.techuni.ui.theme.screens.News
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-
-@Composable
-fun NewsScreen(navController: NavController){
-
-}
---
+++ app/src/main/java/com/example/techuni/ui/theme/screens/events/EventViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.techuni.ui.theme.screens.events
+
+class EventViewModel {
+}
